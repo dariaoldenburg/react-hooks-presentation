f815deab1a1d5424ac871930515654b8af50dc30
dariaoldenburg <dariaoldenburg@gmail.com>
2019-12-08 18:51:47 +0100

    change App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
+import { ThemeContext, themes } from './ThemeContext/ThemeContext';
 
 // import Example from './components/ExampleClass';
 import Example from './components/ExampleHooks';
 
 function App() {
   return (
+    <ThemeContext.Provider value={themes.dark}>
     <div className="App">
-      <header className="App-header">
         <Example />
-      </header>
     </div>
+    </ThemeContext.Provider>
   );
 }
 
