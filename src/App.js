1d8e6d76a6e161f9215278495b67ac9d04ef8051
dariaoldenburg <dariaoldenburg@gmail.com>
2019-12-08 23:56:44 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import { ThemeContext, themes } from './ThemeContext/ThemeContext';
 
 // import Example from './components/ExampleClass';
 // import Example from './components/ExampleHooks';
-import Example from './components/ExampleUseContext';
+// import Example from './components/ExampleUseContext';
+// import Example from './components/ExampleUseReducer';
+// import Example from './components/ExampleUseMemoUseCallback';
+// import Example from './components/ExampleUseMemo';
+import Example from './components/ExampleUseRef';
 
 function App() {
   return (
     <ThemeContext.Provider value={themes.dark}>
       <div className="App">
         <Example />
+        {/* useReducer */}
+        {/* <Example initialCount={2} /> */}
       </div>
     </ThemeContext.Provider>
   );
